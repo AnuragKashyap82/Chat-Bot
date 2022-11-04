1e5885dad4328b46a72700a44cf1245e2f330ccf
Anurag Kashyap <anuragkashyap815353@gmail.com>
2022-11-04 19:17:04 +0530

    Chat Bot App

--- app/src/main/java/kashyap/anurag/botchats/ImageViewActivity.java
+++ app/src/main/java/kashyap/anurag/botchats/ImageViewActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import kashyap.anurag.botchats.databinding.ActivityImageViewBinding;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -13,6 +17,9 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class ImageViewActivity extends AppCompatActivity {
 
@@ -22,7 +29,7 @@ public class ImageViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding  =ActivityImageViewBinding.inflate(getLayoutInflater());
+        binding  = ActivityImageViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         binding.progressBar.setVisibility(View.VISIBLE);
@@ -62,5 +69,60 @@ public class ImageViewActivity extends AppCompatActivity {
                     public void onFailure(@NonNull Exception e) {
                     }
                 });
+        binding.downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadImage(imageUrl);
+            }
+        });
+    }
+
+    private void downloadImage(String imageUrl) {
+
+        long timestamp = System.currentTimeMillis();
+        String nameWithExtension = timestamp + ".png";
+
+        ProgressDialog progressDialog = new ProgressDialog(ImageViewActivity.this);
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setMessage("Downloading" + nameWithExtension + "....");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(imageUrl);
+        storageReference.getBytes(50000000)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        saveDownloadedBook(ImageViewActivity.this, progressDialog, bytes, nameWithExtension);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(ImageViewActivity.this, "Failed to download due to" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private  void saveDownloadedBook(Context context, ProgressDialog progressDialog, byte[] bytes, String nameWithExtension) {
+        try {
+            File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            downloadsFolder.mkdir();
+
+            String filePath = downloadsFolder.getPath() + "/" + nameWithExtension;
+
+            FileOutputStream out = new FileOutputStream(filePath);
+            out.write(bytes);
+            out.close();
+
+            Toast.makeText(context, "Saved to Download Folder", Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
+
+        } catch (Exception e) {
+            Toast.makeText(context, "Failed saving to download folder due to" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
+        }
     }
 }
